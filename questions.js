e6b76f23634a429a7ba49d97612bffb8f53d69ca
niliak0401 <niliak0401@gmail.com>
2026-07-27 20:40:59 +0800

    feat: add question data structure

---
+++ questions.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+        id: 1,
+
+        question:
+        "Which city is known as the Spring City?",
+
+        options: [
+            "Beijing",
+            "Kunming",
+            "Shanghai",
+            "Guangzhou"
+        ],
+
+        answer:
+        "Kunming",
+
+        explanation:
+        "Kunming is called the Spring City because of its mild climate.",
+
+        memoryTip:
+        "昆明气候温和，四季如春 → Spring City"
+    }
+];
