d575d57173a390a08f5812415b0ffd5cff4cd609
niliak0401 <niliak0401@gmail.com>
2026-07-27 21:32:54 +0800

    feat: support multiple questions

--- questions.js
+++ questions.js
@@ -1,26 +1,53 @@
 const questions = [
-    {
-        id:1,
 
-        question:
-        "Which city is known as the Spring City?",
+{
+    id:1,
 
-        options:[
-            "A. Beijing",
-            "B. Kunming",
-            "C. Shanghai",
-            "D. Guangzhou"
-        ],
+    question:
+    "Which city is known as the Spring City?",
 
-        answer:
+    options:[
+        "A. Beijing",
         "B. Kunming",
+        "C. Shanghai",
+        "D. Guangzhou"
+    ],
 
-        explanation:
-        "Kunming is called the Spring City because of its mild climate.",
+    answer:
+    "B. Kunming",
 
-        memoryTip:
-        "昆明气候温和，四季如春 → Spring City"
-    }
+    explanation:
+    "Kunming is called the Spring City because of its mild climate.",
+
+    memoryTip:
+    "昆明气候温和，四季如春 → Spring City"
+},
+
+
+{
+    id:2,
+
+    question:
+    "Which city is famous for the Terracotta Warriors?",
+
+    options:[
+        "A. Xi'an",
+        "B. Beijing",
+        "C. Chengdu",
+        "D. Nanjing"
+    ],
+
+    answer:
+    "A. Xi'an",
+
+    explanation:
+    "The Terracotta Army is located near Xi'an.",
+
+    memoryTip:
+    "兵马俑 → 西安 Xi'an"
+}
+
 ];
+
 
 export default questions;
